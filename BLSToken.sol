672541d40d9aee3fafbd9336650ae99b791359f4
RAKESH UMMADI <104605032+RAKESH9494@users.noreply.github.com>
2024-09-26 18:23:59 +0530

    Update BLSToken.sol

--- BLSToken.sol
+++ BLSToken.sol
@@ -1,15 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-// BLS Token (ERC20) - Liquid staking token
-contract BLSToken is ERC20{
-    // constructor() ERC20("Blume Liquid Staking", "BLS") Ownable(msg.sender) {
-    //     _mint(address(this), 1000); // Initial supply 
-    // }
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-    constructor() ERC20("Blume Liquid Staking", "BLS") {
-        _mint(address(this), 1000); // Initial supply 
+// BLS Token (ERC20) - Liquid staking token
+contract BLSToken is ERC20, Ownable{
+    constructor() ERC20("Blume Liquid Staking", "BLS") Ownable(msg.sender) {
+        _mint(address(this), 1000*(10 ** decimals())); // Initial supply 
     }
 
     // Buy BLS tokens 
@@ -20,6 +18,6 @@ contract BLSToken is ERC20{
 
     //Transfer tokens
     function transferToken(address from, address to, uint amount) public  {
-        _transfer(from, to, amount);
+        _transfer(from, to, amount*(10 ** decimals()));
     }
 }
